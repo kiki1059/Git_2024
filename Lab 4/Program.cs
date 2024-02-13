23ad6c01fe2c244f61dfcb61bd77f09de3bd7fcc
Kikiloretioluwa Amosu <S00248440@atu.ie>
2024-02-13 17:37:32 +0000

    done

--- Lab 4/Program.cs
+++ Lab 4/Program.cs
@@ -1,10 +1,59 @@
-﻿namespace Lab_4
+﻿using System.Text;
+
+namespace Lab_4
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(IsValidEmployeeId("E12345")== true);
+            Console.WriteLine(IsValidEmployeeId("E123") == false);
+            Console.WriteLine(IsValidEmployeeId("P12345") == false);
+            Console.WriteLine(IsValidEmployeeId("") == false);
+
+            Console.WriteLine(IsValidHoursWorked(20) == true);
+
+            Console.WriteLine(IsValidPayRate(5) == true);
+
         }
+
+        static bool IsValidEmployeeId(string input)
+        {
+         if (input.Length  != 6) 
+            {
+                return false;
+            }
+
+         if (input[0] != 'E')
+            {
+                return false;
+            }
+
+            return true;
+            
+        }
+
+        static bool IsValidHoursWorked(double hours)
+        {
+            if (hours > 10 && hours < 50)
+                {
+                    return true;
+                }
+
+                return false;
+        }
+
+        static bool IsValidPayRate(double PayRate)
+        {
+            if (PayRate !> 10 && PayRate !< 65)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+
+
     }
 }
